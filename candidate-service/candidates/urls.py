f189f332091c08fb597f6519b98a5a3bcf25618a
RaviRajGhorsai <graviraj344@gmail.com>
2026-08-25 18:09:21 +0545

    urls for job application, search view added.

--- candidate-service/candidates/urls.py
+++ candidate-service/candidates/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
 from candidates.views.candidate_view import CandidateViewSet
+from candidates.views.job_search_view import JobSearchViewSet
+from candidates.views.job_application_view import JobApplicationViewSet
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'candidates', CandidateViewSet, basename='candidate')
 
+router.register(r'candidates', CandidateViewSet, basename='candidate')
+router.register(r'jobs', JobSearchViewSet, basename='job-search')
+router.register(r'applications', JobApplicationViewSet, basename='job-application')
 
 urlpatterns = router.urls
