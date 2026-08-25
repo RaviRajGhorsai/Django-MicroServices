d54c324cf0f9a5f05a0bd6bdf0d78155bbdfc47b
RaviRajGhorsai <graviraj344@gmail.com>
2026-08-25 18:20:39 +0545

    urls for job search renamed from /jobs to /search/job

--- candidate-service/candidates/urls.py
+++ candidate-service/candidates/urls.py
@@ -7,7 +7,7 @@ from candidates.views.job_application_view import JobApplicationViewSet
 router = DefaultRouter(trailing_slash=False)
 
 router.register(r'candidates', CandidateViewSet, basename='candidate')
-router.register(r'jobs', JobSearchViewSet, basename='job-search')
+router.register(r'search/jobs', JobSearchViewSet, basename='job-search')
 router.register(r'applications', JobApplicationViewSet, basename='job-application')
 
 urlpatterns = router.urls
