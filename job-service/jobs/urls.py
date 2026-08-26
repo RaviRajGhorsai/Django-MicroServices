fb2ed17136b3fbc23e13c745aa8ed8b7b5851fcf
RaviRajGhorsai <graviraj344@gmail.com>
2026-08-26 14:46:04 +0545

    urls for auth viewset added.

--- job-service/jobs/urls.py
+++ job-service/jobs/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from jobs.views.job_view import JobViewSet
 from jobs.views.application_view import ApplicationViewSet
+from jobs.views.auth_viewset import AuthViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'jobs', JobViewSet, basename='job')
 router.register(r'applications', ApplicationViewSet, basename='application')
+router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = router.urls
