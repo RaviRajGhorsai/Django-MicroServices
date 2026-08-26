67b6c19aa79e7f5e471c023ca6886742529f6614
RaviRajGhorsai <graviraj344@gmail.com>
2026-08-26 20:37:46 +0545

    error found while testing--> status field in application serializer was made read only so it was not updating in DB-- fixed.

--- job-service/jobs/serializers/application_serializer.py
+++ job-service/jobs/serializers/application_serializer.py
@@ -5,19 +5,19 @@ from jobs.models import Application
 class ApplicationSerializer(serializers.ModelSerializer):
 
     # Expose nested candidate info cleanly in API responses
-    candidate_name  = serializers.SerializerMethodField()
-    candidate_email = serializers.SerializerMethodField()
-    candidate_skills = serializers.SerializerMethodField()
+    # candidate_name  = serializers.SerializerMethodField()
+    # candidate_email = serializers.SerializerMethodField()
+    # candidate_skills = serializers.SerializerMethodField()
 
     class Meta:
         model = Application
         fields = [
             'id', 'job', 'candidate_id', 'candidate_data',
-            'candidate_name', 'candidate_email', 'candidate_skills',
+            # 'candidate_name', 'candidate_email', 'candidate_skills',
             'cover_letter', 'status', 'applied_at', 
         ]
         read_only_fields = [
-            'candidate_data', 'status', 'applied_at',
+            'candidate_data', 'applied_at',
         ]
 
     def get_candidate_name(self, obj):
