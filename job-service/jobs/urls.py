d740fe71921bca5f71d389510d973417bd1480ad
RaviRajGhorsai <graviraj344@gmail.com>
2026-08-29 12:55:39 +0545

    url added for new candidate view

--- job-service/jobs/urls.py
+++ job-service/jobs/urls.py
@@ -2,10 +2,12 @@ from rest_framework.routers import DefaultRouter
 from jobs.views.job_view import JobViewSet
 from jobs.views.application_view import ApplicationViewSet
 from jobs.views.auth_viewset import AuthViewSet
+from jobs.views.candidate_view import CandidateView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'jobs', JobViewSet, basename='job')
 router.register(r'applications', ApplicationViewSet, basename='application')
+router.register(r'candidate', CandidateView, basename='candidate')
 router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = router.urls
