0c01ffd11734d9fe91b0c70ad7a02fb87e147134
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-09 15:51:45 +0545

    cors allow update.

--- candidate-service/config/settings.py
+++ candidate-service/config/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ["candidate.pratik.info.np", "localhost", "127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://candidate.pratik.info.np",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
