bd898ba71d3802d3c3618b82f68bc983aae52cf7
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-09 15:20:02 +0545

    minio region added.

--- candidate-service/candidates/services/minio.py
+++ candidate-service/candidates/services/minio.py
@@ -19,6 +19,7 @@ public_client = Minio(
     access_key=settings.MINIO_ACCESS_KEY,
     secret_key=settings.MINIO_SECRET_KEY,
     secure=settings.MINIO_PUBLIC_SECURE,
+    region="us-east-1",
 )
 
 
