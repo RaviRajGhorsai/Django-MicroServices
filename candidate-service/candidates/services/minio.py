0acbd284f2d5796d93f3f5a24a96891504fc3cce
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-09 14:59:09 +0545

    minio public endpoint added.

--- candidate-service/candidates/services/minio.py
+++ candidate-service/candidates/services/minio.py
@@ -4,31 +4,35 @@ from minio import Minio
 from django.conf import settings
 
 
-client = Minio(
+# Django → MinIO
+internal_client = Minio(
     settings.MINIO_ENDPOINT,
     access_key=settings.MINIO_ACCESS_KEY,
     secret_key=settings.MINIO_SECRET_KEY,
     secure=settings.MINIO_SECURE,
 )
 
-#public_client = Minio(
-#    settings.MINIO_PUBLIC_ENDPOINT,
-#    access_key=settings.MINIO_ACCESS_KEY,
-#    secret_key=settings.MINIO_SECRET_KEY,
-#    secure=settings.MINIO_PUBLIC_SECURE,
-#)
+
+# Browser-facing presigned URLs
+public_client = Minio(
+    settings.MINIO_PUBLIC_ENDPOINT,
+    access_key=settings.MINIO_ACCESS_KEY,
+    secret_key=settings.MINIO_SECRET_KEY,
+    secure=settings.MINIO_PUBLIC_SECURE,
+)
+
 
 def ensure_bucket_exists():
     bucket = settings.MINIO_BUCKET
 
-    if not client.bucket_exists(bucket):
-        client.make_bucket(bucket)
+    if not internal_client.bucket_exists(bucket):
+        internal_client.make_bucket(bucket)
 
 
 def generate_upload_url(object_name):
     ensure_bucket_exists()
 
-    return client.presigned_put_object(
+    return public_client.presigned_put_object(
         settings.MINIO_BUCKET,
         object_name,
         expires=timedelta(minutes=10),
@@ -36,7 +40,7 @@ def generate_upload_url(object_name):
 
 
 def generate_download_url(object_name):
-    return client.presigned_get_object(
+    return public_client.presigned_get_object(
         settings.MINIO_BUCKET,
         object_name,
         expires=timedelta(minutes=10),
