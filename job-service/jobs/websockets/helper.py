9a488785dbadad59cef421863d80e3cfd4449d6e
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-10 15:14:39 +0545

    helper update.

--- job-service/jobs/websockets/helper.py
+++ job-service/jobs/websockets/helper.py
@@ -2,11 +2,11 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-def send_websocket_notification(candidate_id, data):
+def send_websocket_notification(user_id, data):
     channel_layer = get_channel_layer()
 
     async_to_sync(channel_layer.group_send)(
-        f"candidate_{candidate_id}",
+        f"HR_user_{user_id}",
         {
             "type": "notification",
             "data": data,
