3e253ef8a89cc129717346bb458fb56043d596a4
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-10 14:58:30 +0545

    asgi server config added.

--- job-service/config/asgi.py
+++ job-service/config/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from jobs.routing impoer websocket_urlpatterns
+
+
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
