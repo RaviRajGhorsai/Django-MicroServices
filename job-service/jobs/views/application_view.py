b4de1dad2cc6fe6854e65ac2aa0eb0cc7ad3af95
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-10 17:37:32 +0545

    job title field added in application status update event.

--- job-service/jobs/views/application_view.py
+++ job-service/jobs/views/application_view.py
@@ -168,6 +168,7 @@ class ApplicationViewSet(viewsets.ViewSet):
                     "event_type": "application.status_updated",
                     "job_id": instance.job_id,
                     "candidate_id": instance.candidate_id,
+                    "job_title": instance.job.title,
                     "application_id": instance.id,
                     "new_status": new_status,
                 },
@@ -206,6 +207,7 @@ class ApplicationViewSet(viewsets.ViewSet):
                         "job_id": instance.job_id,
                         "candidate_id": instance.candidate_id,
                         "application_id": instance.id,
+                        "job_title": instance.job.title,
                         "new_status": new_status,
                     },
                 )
