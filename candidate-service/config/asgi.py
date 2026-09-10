0b808ff0ab764c3294fed1a84dc05bb488f88ca1
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-10 14:12:56 +0545

    env acces fix

--- candidate-service/config/asgi.py
+++ candidate-service/config/asgi.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from candidates.routing import websocket_urlpatterns
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
