0aae6df8047a94f317ffefad91a28221252b39ac
RaviRajGhorsai <graviraj344@gmail.com>
2026-09-10 15:49:16 +0545

    logging added.

--- job-service/jobs/websockets/helper.py
+++ job-service/jobs/websockets/helper.py
@@ -4,9 +4,14 @@ from channels.layers import get_channel_layer
 
 def send_websocket_notification(user_id, data):
     channel_layer = get_channel_layer()
+    
+    group_name = f"HR_user_{user_id}"
+
+    print(f"WS SEND GROUP: {group_name}")
+    print(f"WS SEND DATA: {data}")
 
     async_to_sync(channel_layer.group_send)(
-        f"HR_user_{user_id}",
+        group_name,
         {
             "type": "notification",
             "data": data,
